8e12c877aa9912d51cacb6046bb22c2d880b4f4f
sviatoslav <SvyatoslavPud@gmail.com>
2023-10-27 12:18:16 +0300

    Rename HomePage.js to Promo.js and fix CSS

---
+++ src/content/promo/Promo.js
@@ -0,0 +1,19 @@
+import socks from "../../img/socks.svg";
+import style from './promo.module.scss'
+
+const Promo = () => {
+    return (
+        <section className={style.promo}>
+            <div className={style.promoMain}>
+                <h2 className={style.promoSubtitle}>Благодійний мерч</h2>
+                <h1 className={style.promoTitle}>Бандерогусь –<br/>за сраку орків кусь</h1>
+                <p>Інвестуй у стильний гардероб, перемогу України та бавовну тим,
+                    хто вапщє нє хочєт атсюда уєзжать, бо тут так класна, как в свайом домє</p>
+                <button className={style.promoBtn}><a href='#products'>Інвестувати у бавовну</a></button>
+            </div>
+            <img src={socks} alt='socks' align='right'/>
+        </section>
+    )
+}
+
+export {Promo}
