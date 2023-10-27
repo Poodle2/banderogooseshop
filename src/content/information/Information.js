35199672042b95fbf446d2e49207d19bc065701c
sviatoslav <SvyatoslavPud@gmail.com>
2023-10-27 12:19:59 +0300

    Rename blockInformation.js to Information.js and fix CSS

--- src/content/information/Information.js
+++ src/content/information/Information.js
@@ -1,36 +1,36 @@
-import style from './blockInformation.module.scss'
+import style from './information.module.scss'
 import flag from "../../img/information/flag.svg";
 import people from "../../img/information/people.svg";
 
-const BlockInformation = () => {
+const Information = () => {
     return (
-        <div className={style.BrandInformation}>
-            <div id='brandInformation'>
+        <section className={style.information} id='brandInformation'>
+            <div>
                 <h2>Про бренд</h2>
-                <div className={style.blocks}>
-                    <div className={style.donatesBlock}>
-                        <img className={style.flag} src={flag} alt='flag' width={546} height={541}/>
-                        <div className={style.textBlock}>
+                <div className={style.informationBlocks}>
+                    <div className={style.informationDonate}>
+                        <img className={style.informationFlag} src={flag} alt='flag' width={546} height={541}/>
+                        <div className={style.informationDesc}>
                             <h3>50% на ЗСУ</h3>
                             <p>Мета нашого бренду – доносити позитивний меседж бандерогуся, підтримати українських
                                 виробників і
                                 зібрати ще більше донатів на перемогу</p>
                         </div>
                     </div>
-                    <div className={style.stylesBlock}>
-                        <div className={style.textBlock}>
+                    <div className={style.informationStyles}>
+                        <div className={style.informationDesc}>
                             <h3>Стиль переможців</h3>
                             <p>Бандерогусак одразу перенесе тебе до ліги Високої моди Haut Couture. Так і стають синами
                                 та
                                 доньками
                                 тієї самої маминої подруги </p>
                         </div>
-                        <img className={style.people} src={people} alt='people' width={463} height={484}/>
+                        <img className={style.informationPeople} src={people} alt='people' width={463} height={484}/>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
-export {BlockInformation}
+export {Information}
